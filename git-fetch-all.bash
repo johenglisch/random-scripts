1cc1e813abc25cd3fba4d055d824efcc38f41358
Johannes Englisch <cyberjoe0815@hotmail.com>
2017-11-03 08:56:57 +0100

    inlined function

--- git-fetch-all.bash
+++ git-fetch-all.bash
@@ -3,13 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-function fetch-repo() {
-    echo "Fetching '$*'"
-    (cd "$*" && git fetch > /dev/null && git status -sb) || (echo "Failed!" >&2)
-    echo
-}
-
-
 if [ -z "${REPOS:-}" ]
 then
     echo 'Error: $REPOS is not set.' >&2
@@ -19,5 +12,7 @@ fi
 IFS=';'
 for dir in $REPOS
 do
-    fetch-repo "$dir"
+    echo "Fetching '$dir'"
+    (cd "$dir" && git fetch > /dev/null && git status -sb) || (echo "Failed!" >&2)
+    echo
 done
